b18a674d5174163815fac59f0fd7a5e1f75fe013
Michael Carr <mcarr2468@gmail.com>
2021-09-06 16:47:37 -0500

    V1.0 starting html & css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import questionsData from "./questionsData"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { AiOutlinePlus } from 'react-icons/ai';
+import { AiOutlineMinus } from  'react-icons/ai';
+
+export const App = () => {
+
+    return (
+        <section className="container">
+          <h1 className="container-heading">Questions And Answers About Login</h1>
+
+          <ul className="questions">
+            <li className="question">
+              <header className="question__header">
+                <h2>Do I have to allow the use of cookes?</h2>
+                <button className="question__open-btn"><AiOutlinePlus/></button>
+              </header>
+              <p className="question__text show-question-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque adipisci nisi est assumenda iure cumque.Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque adipisci nisi est assumenda iure cumque.Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque adipisci nisi est assumenda iure cumque.</p>
+            </li>
+
+            <li className="question">
+              <header className="question__header">
+                <h2>Do I have to allow the use of cookes?</h2>
+                <button className="question__open-btn"><AiOutlinePlus/></button>
+              </header>
+              <p className="question__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque adipisci nisi est assumenda iure cumque.Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque adipisci nisi est assumenda iure cumque.Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque adipisci nisi est assumenda iure cumque.</p>
+            </li>
+
+            <li className="question">
+              <header className="question__header">
+                <h2>Do I have to allow the use of cookes?</h2>
+                <button className="question__open-btn"><AiOutlinePlus/></button>
+              </header>
+              <p className="question__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque adipisci nisi est assumenda iure cumque.Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque adipisci nisi est assumenda iure cumque.Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque adipisci nisi est assumenda iure cumque.</p>
+            </li>
+          </ul>
+        </section>
+    )
 }
-
-export default App;
